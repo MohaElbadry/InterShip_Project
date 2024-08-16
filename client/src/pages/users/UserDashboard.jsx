51c42b75d2e2cd165c05a982787a1491b11992a2
mohammed el badry <92531732+MohaElbadry@users.noreply.github.com>
2024-08-16 01:22:24 +0100

    chore: Add header component to UserDashboard page

--- client/src/pages/users/UserDashboard.jsx
+++ client/src/pages/users/UserDashboard.jsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+import Header from "../../components/layout/Header";
 
 export default function UserDashboard() {
   return (
     <>
+      <Header />
       <h1>USER DACH</h1>
     </>
   );
